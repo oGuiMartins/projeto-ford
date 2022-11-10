d350599f72e4c845ffdfd200d54feb858cb4bda2
Guilherme Martins <95319668+oGuiMartins@users.noreply.github.com>
2022-11-10 17:31:00 -0300

    debug
    debug=False

--- app.py
+++ app.py
@@ -156,4 +156,4 @@ def api(val):
 if __name__=='__main__': 
     port = 5000 + random.randint(0, 999)
     url = "http://127.0.0.1:{0}".format(port)
-    app.run(use_reloader=False, debug=True, port=port)
+    app.run(use_reloader=False, debug=False, port=port)
